950fccb0c5e90ef16c686ee7ca623a9240b7e54f
Bánka Milán <bankamilan22@gmail.com>
2018-09-21 12:07:57 +0200

    Get BDO DB
    END

--- Csgetrekt.cs
+++ Csgetrekt.cs
@@ -11,7 +11,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication7
+namespace WindowsFormsApplication8
 {
     public partial class Form1 : Form
     {
@@ -28,63 +28,81 @@ namespace WindowsFormsApplication7
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
             string code;
             string s;
-            string[] val = new string[] {""};
-            bool lcb = false;
-            for (int x = 1; x < 50000; x++)
+            string line;
+            string[] val = new string[] { "" };
+            bool k = false;
+            bool f = false;
+            bool m = false;
+            for (int x = 1; x < 100000; x++)
             {
-                string urlDemo = "https://bddatabase.net/us/item/"+x+"/";
+                string urlDemo = "https://bddatabase.net/us/item/" + x + "/";
                 WebRequest request = WebRequest.Create(urlDemo);
                 request.Proxy.Credentials = CredentialCache.DefaultCredentials;
                 WebResponse response = request.GetResponse();
-                //STATUS
-                //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
                 Stream dataStream = response.GetResponseStream();
                 StreamReader reader = new StreamReader(dataStream);
-                string line;
-                string kk = "meta name=\"description\"";
                 //string m = reader.ReadToEnd();
-                //File.AppendAllText(@"U:\id.txt", m);
-                for (int i = 0; i < 800; i++)
+                //File.AppendAllText(@"U:\lo.txt",m + Environment.NewLine);
+                f = false;
+                for (int i = 0; i < 11; i++)
                 {
                     line = reader.ReadLine();
-                    if (i<=10)
+                    if (line.Contains("meta name=\"description\""))
                     {
-                        if (line.Contains(kk))
-                        {
-                            string toBeSearched = "Id: ";
-                            int ix = line.IndexOf(toBeSearched);
+                        f = true;
+                        string toBeSearched = "Id: ";
+                        int ix = line.IndexOf(toBeSearched);
 
-                            if (ix != -1)
-                            {
-                                code = line.Substring(ix + toBeSearched.Length);
-                                s = code.Substring(0, code.IndexOf("."));
-                                val = s.Split(new string[] { "-" }, StringSplitOptions.None);
-                               
-                            }
+                        if (ix != -1)
+                        {
+                            code = line.Substring(ix + toBeSearched.Length);
+                            s = code.Substring(0, code.IndexOf("."));
+                            val = s.Split(new string[] { "-" }, StringSplitOptions.None);
+
                         }
                     }
-                    if (i >= 500 && i <= 800)
+                    else
                     {
+                        k = true;
+                    }
+                }
+                if (f)
+                {
+                    k = false;
+                    for (int i = 10; i < 700; i++)
+                    {
+                            line = reader.ReadLine();
                             if (line.Contains("Bound"))
                             {
-                                lcb = true;
+                                k = true;
+                            }
+                            if (line.Contains("Market Price"))
+                            {
+                                m = true;
                             }
                     }
-                  
+                }
 
+
+
+                if (!k && !m)
+                {// "ID: " + val[0] + "   Name: " + 
+                    File.AppendAllText(@"U:\idname.txt",val[1] + Environment.NewLine);
                 }
-                if (!lcb)
-                {
-                    File.AppendAllText(@"U:\idname.txt", "ID: " + val[0] + "   Name: " + val[1] + Environment.NewLine);
-                    File.AppendAllText(@"U:\id.txt", val[0] + Environment.NewLine);
-                    File.AppendAllText(@"U:\name.txt", val[1] + Environment.NewLine);
-                }
-                lcb = false;
+
+                k = false;
+                m = false;
                 reader.Close();
                 response.Close();
-
+                label1.Text = x.ToString();
+                label1.Refresh();
             }
         }
     }
